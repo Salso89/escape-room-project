532ca01359d4fdf233b8da7cd35261b195f964ff
Shane Sally <122490075+Salso89@users.noreply.github.com>
2023-07-23 20:54:22 +0100

    Update sockets-server.py

--- sockets-server.py
+++ sockets-server.py
@@ -1,45 +1,66 @@
-import PySimpleGUI as sg
+import RPi.GPIO as GPIO
+import random
+import functools
 import socket
 
-# IP address and port of the first Raspberry Pi
-SERVER_IP = '127.0.0.1'  # Replace with the actual IP address
-SERVER_PORT = 8888  # Replace with the actual port number
+# If code is stopped during active it will stay active
+# This may produce a warning if restarted, this
+# line prevents that.
+GPIO.setwarnings(False)
+# This means we will refer to the GPIO
+# by the number after GPIO.
+GPIO.setmode(GPIO.BCM)
+# This sets up the GPIO 18 pin as an output pin
+GPIO.setup(18, GPIO.OUT)
+# Turns Relay On. Brings Voltage to Min GPIO can output ~0V. (Sets lock)
+GPIO.output(18, 1)
 
-# Create a socket for communication
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+code = [] # Code that needs to be displayed on the UI as the final sequence is randomly generated using a random function
+for _ in range(5):
+    randomNumber2 = random.randint(0, 9)
+    code.append(randomNumber2)
+print("Code is", code)
 
-# Connect to the server (first Raspberry Pi)
-client_socket.connect((SERVER_IP, SERVER_PORT))
+numbers = [0, 0, 0, 0, 0]
 
-# Set up the PySimpleGUI interface
-sg.theme('DarkAmber')
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-layout = [
-    [sg.Text('Enter the code:', font=('Helvetica', 16))],
-    [sg.Input(size=(10, 1), key='-CODE-', font=('Helvetica', 16))],
-    [sg.Button('Submit', key='-SUBMIT-', font=('Helvetica', 16))]
-]
+# Bind the socket to a specific address and port (replace 'SERVER_IP' and 'SERVER_PORT' with the actual IP and port you want to use)
+server_ip = '127.0.0.1'
+server_port = 65432
+server_socket.bind((server_ip, server_port))
 
-window = sg.Window('Lock Control', layout)
+# Listen for incoming connections (1 connection in the queue)
+server_socket.listen(1)
+
+# Accept a connection from the client
+print("Waiting for a connection from the client...")
+client_socket, client_address = server_socket.accept()
+print(f"Connection established with {client_address}")
 
 while True:
-    event, values = window.read()
-
-    if event == sg.WINDOW_CLOSED:
+    event = client_socket.recv(1024).decode()
+    if not event:
         break
 
-    if event == '-SUBMIT-':
-        code = values['-CODE-']
+    if event == 'Exit':
+        break
 
-        # Send the code to the server (first Raspberry Pi)
-        client_socket.send(code.encode())
+    button_number = int(event[-1])
+    numbers[button_number] += 1
+    if numbers[button_number] > 9:
+        numbers[button_number] = 0
 
-        # Receive the verification result from the server
-        verification_result = client_socket.recv(1024).decode()
+    print("Numbers:", numbers)
 
-        if verification_result == 'CORRECT':
-            sg.popup('Code is correct. Lock opened.')
-        else:
-            sg.popup('Code is incorrect.')
+    if functools.reduce(lambda x, y: x and y, map(lambda p, q: p == q, numbers, code), True):
+        print("Numbers and code are the same")
+        GPIO.output(18, 0)  # This Turns Relay Off. Brings Voltage to Max GPIO can output ~3.3V (Opens lock)
+        break
+    else:
+        print("Numbers and code are not the same")
 
-window.close()
+# Close the sockets
+client_socket.close()
+server_socket.close()
