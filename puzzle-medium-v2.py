31b9c7c8e1ccff29962c2290a07d76a4265c25c3
Shane Sally <122490075+Salso89@users.noreply.github.com>
2023-08-15 16:39:34 +0100

    Update puzzle-medium-v2.py
    Updated Medium UI formatted to fit 3.5inch screen

--- puzzle-medium-v2.py
+++ puzzle-medium-v2.py
@@ -11,30 +11,29 @@ numbers = [0,0,0,0,0] # Numbers that are initially displayed on the UI are rando
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server (replace 'SERVER_IP' and 'SERVER_PORT' with the actual IP and port of the server)
-server_ip = '127.0.0.1'
-server_port = 65455
+server_ip = '192.168.0.135'
+server_port = 65450
 client_socket.connect((server_ip, server_port))
 
 # Layout of the UI is established using a series of container objects called frames that have been called using the PySimpleGUI import
-layout = [
-    
-    [sg.T('   Enter Code below ... ', font=('Krungthep', 10))],
-    [
-        sg.Frame('', element_justification='center', layout=[[sg.T(numbers[0], font=("Krungthep", 60), key='Number0', pad=(
-            10, 10))], [sg.Button('PRESS', key="Button0", font=('Kohinoor Telugu', 10))]]),
-        sg.Frame('', element_justification='center', layout=[[sg.T(numbers[1], font=("Krungthep", 60), key='Number1', pad=(
-            10, 10))], [sg.Button('PRESS', key="Button1", font=('Kohinoor Telugu', 10))]]),
-        sg.Frame('', element_justification ='center', layout=[[sg.T(numbers[2], font=("Krungthep", 60), key='Number2', pad=(
-            10, 10))], [sg.Button('PRESS', key="Button2", font=('Kohinoor Telugu', 10))]]),
-        sg.Frame('', element_justification='center', layout=[[sg.T(numbers[3], font=("Krungthep", 60), key='Number3', pad=(
-            10, 10))], [sg.Button('PRESS', key="Button3", font=('Kohinoor Telugu', 10))]]),
-        sg.Frame('', element_justification='center', layout=[[sg.T(numbers[4], font=("Krungthep", 60), key='Number4', pad=(
-            10, 10))], [sg.Button('PRESS', key="Button4", font=('Kohinoor Telugu', 10))]])
-    ],
-    
+layout = [[sg.Frame('', layout=[], size=(1, 10))], [sg.T('   Enter Code below ... ', font=('Krungthep', 30), pad=(0, 30))],
+
+[
+    sg.Frame('', element_justification='center', layout=[[sg.T(numbers[0], font=("Krungthep", 90), key='Number0', pad=(
+        10, 10))], [sg.Button('PRESS', key="Button0", font=('Kohinoor Telugu', 17), size=(5,2))]]),
+    sg.Frame('', element_justification='center', layout=[[sg.T(numbers[1], font=("Krungthep", 90), key='Number1', pad=(
+        10, 10))], [sg.Button('PRESS', key="Button1", font=('Kohinoor Telugu', 17), size=(5,2))]]),
+    sg.Frame('', element_justification ='center', layout=[[sg.T(numbers[2], font=("Krungthep", 90), key='Number2', pad=(
+        10, 10))], [sg.Button('PRESS', key="Button2", font=('Kohinoor Telugu', 17), size=(5,2))]]),
+    sg.Frame('', element_justification='center', layout=[[sg.T(numbers[3], font=("Krungthep", 90), key='Number3', pad=(
+        10, 10))], [sg.Button('PRESS', key="Button3", font=('Kohinoor Telugu', 17), size=(5,2))]]),
+    sg.Frame('', element_justification='center', layout=[[sg.T(numbers[4], font=("Krungthep", 90), key='Number4', pad=(
+        10, 10))], [sg.Button('PRESS', key="Button4", font=('Kohinoor Telugu', 17), size=(5,2))]])
+],
+
 ]
 
-window = sg.Window("Key Pad", layout, size=(480, 320), element_justification='center', finalize=True)
+window = sg.Window("Key Pad", layout, size=(680, 520), element_justification='center', finalize=True)
 window.Maximize()
 
 while True:
@@ -90,4 +89,3 @@ while True:
             client_socket.send(client_event.encode())
 
 window.close()
-
